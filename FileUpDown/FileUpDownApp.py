9dd8757da06cacfb94d03598375af6ddcbb321a6
RengeRenge <lidrkuft123@163.com>
2021-01-11 03:22:48 +0800

    feature: 添加新接口

--- FileUpDown/FileUpDownApp.py
+++ FileUpDown/FileUpDownApp.py
@@ -9,13 +9,13 @@ Actually, it launches as a Flask application, for web service request handing.
 import os
 
 from flask import Flask
-from werkzeug.contrib.fixers import ProxyFix
+# from werkzeug.contrib.fixers import ProxyFix
 
 import GlobalConfigContext
 from Gateway import FileGateway
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
+# app.wsgi_app = ProxyFix(app.wsgi_app)
 app.register_blueprint(FileGateway.RestRouter)
 app.config['SECRET_KEY'] = 'niang_pa_si'
 
@@ -23,5 +23,5 @@ if os.path.exists(GlobalConfigContext.FileStore_Directory) is not True:
     os.mkdir(GlobalConfigContext.FileStore_Directory)
 
 if __name__ == '__main__':
-    app.debug = True
+    # app.debug = True
     app.run()
--- FileUpDown/Service/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-This package contains modules of controllers or service handlers.
-All requests from Gateway will be passed here for handling and get
-synchronize return data.
-"""
